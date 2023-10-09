1849af7d32f8204c60f74317ab0f5a175945def4
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-09 11:34:44 +0530

    uplode code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    welcome to the Nandani Ecommerce website
+    </div>
+  );
+}
+
+export default App;
