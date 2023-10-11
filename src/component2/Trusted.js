b1da033771a7ae930f009d0810a7af4ed7803a68
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-11 16:40:58 +0530

    creacte services page

---
+++ src/component2/Trusted.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Trusted = () => {
+  return (
+    <div>
+      Trusted
+    </div>
+  )
+}
+
+export default Trusted
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import HeroSection from "../component2/HeroSection";
+import Services from "../component2/Services";
+import Trusted from "../component2/Trusted";
 
 const Home = () => {
   const data = {
@@ -8,6 +10,8 @@ const Home = () => {
   return (
     <div>
       <HeroSection myData={data} />
+      <Services />
+      <Trusted />
     </div>
   );
 };
