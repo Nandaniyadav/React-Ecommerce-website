e9ea14b41c24ab934fbd31aa8819ded3c3581f8d
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-15 11:54:55 +0530

    Fetch API data using Axios

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,3 @@
-// import React, { useContext } from "react";
-// import HeroSection from "../component2/HeroSection";
-// import { AppContext } from "../context/Productcontext";
-// // import { useProductContext } from "../context/Productcontext";
-
-// const About = () => {
-//   const { myName } = useContext(AppContext);
-//   const data = {
-//     name: "Nandani Ecommerce",
-//   };
-
-//   return (
-//     <div>
-//       {myName}
-//       <HeroSection myData={data} />
-//     </div>
-//   );
-// };
-
-// export default About;
-
 import HeroSection from "../component2/HeroSection";
 import { useProductContext } from "../context/Productcontext";
 
--- src/context/Productcontext.js
+++ src/context/Productcontext.js
@@ -1,21 +1,27 @@
-// import { createContext } from "react";
-
-// const AppContext = createContext();
-
-// const AppProvider = ({ children }) => {
-//   return (
-//     <AppContext.Provider value={{ mayName: "Nandani Yadav" }}>
-//       {children}
-//     </AppContext.Provider>
-//   );
-// };
-// export { AppProvider, AppContext };
-
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 const AppProvider = ({ children }) => {
+  const getProducts = async (url) => {
+    const responce = await axios.get(url);
+
+    const products = await responce.data;
+    console.log(
+      "~ file: productcontext.js ~ line 10 ~ getProduct ~ response",
+      responce
+    );
+  };
+
+  useEffect(() => {
+    getProducts(API);
+
+    return () => {};
+  }, []);
+
   return (
     <AppContext.Provider value={{ myName: "Nandani Yadav" }}>
       {children}
