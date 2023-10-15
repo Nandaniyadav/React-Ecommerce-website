6386f60c92be9cdc93eb135f616fecd473128ac6
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-15 19:11:23 +0530

    Complete Feature Section of Home Page

--- src/component2/Product.js
+++ src/component2/Product.js
@@ -1,58 +1,29 @@
-// import React from 'react'
-// import { NavLink } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom';
 
-// const Product = ({ curElem }) => {
-//   const { id, name, image, price, category } = curElem;
-//   return (
-//     <NavLink to={`/SingleProduct/${id}`}>
-//       <div className="card">
-//         <figure>
-//           <img src={image} alt={name} />
-//           <figcaption className='caption'>{category}</figcaption>
-//         </figure>
-//         <div className="card-data">
-//           <div className="card-data-flex ">
-//             <h3>{name}</h3>
-//             <p className="card-data--price">
-//               {price}
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </NavLink>
-
-//   )
-// }
-
-// export default Product
-
-
-
-import React from "react";
-import { NavLink } from "react-router-dom";
-
-const Product = (curElem) => {
+const Product = (curElem ) => {
   const { id, name, image, price, category } = curElem;
   return (
-    <NavLink to={`/singleproduct/${id}`}>
+    <NavLink to={`/SingleProduct/${id}`}>
       <div className="card">
         <figure>
           <img src={image} alt={name} />
-          <figcaption className="caption">{category}</figcaption>
+          <figcaption className='caption'>{category}</figcaption>
         </figure>
-
         <div className="card-data">
-          <div className="card-data-flex">
+          <div className="card-data-flex ">
             <h3>{name}</h3>
-            <p className="card-data--price">{price}</p>
+            <p className="card-data--price">
+              {price}
+            </p>
           </div>
         </div>
       </div>
     </NavLink>
-  );
-};
 
-export default Product;
+  )
+}
 
+export default Product
 
 
