07261132bc8e0012ff3f2dc57d420312a245d4f2
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-10-27 11:04:45 +0530

    Product Page with all Filters & Sorting 01~

---
+++ src/component2/FilterSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FilterSection = () => {
+  return (
+    <div>
+      Filter
+    </div>
+  )
+}
+
+export default FilterSection
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
+import FilterSection from "../component2/FilterSection";
+import Sort from "../component2/Sort";
+import ProductList from "../component2/ProductList";
 
 const Product = () => {
-  return (
-    <div>
-      Product page
+  return <Wrapper>
+    <div className="container grid grid-filter-column">
+      <div>
+        <FilterSection />
+      </div>
+      <section className="product-view--sort">
+        <div className="sort-filter">
+          <Sort />
+        </div>
+        <div className="main-product">
+          <ProductList />
+        </div>
+      </section>
     </div>
-  )
-}
+  </Wrapper>;
+};
 
-export default Product
+const Wrapper = styled.section`
+  .grid-filter-column {
+    grid-templete-columns: 0.2fr 1fr;
+  }
+  @media (max-width ${({ theme }) => theme.media.mobile}) {
+    .grid-filter-column {
+      grid-templete-columns: 1fr;
+    }
+  }
+`;
+export default Product;
